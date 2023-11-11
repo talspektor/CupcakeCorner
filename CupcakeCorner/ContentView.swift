7a0a8c724cca5f1fd2d91cd5b44e7c765d11f964
Tal Spektor <tal.s@zemingo.com>
2023-11-11 22:12:12 +0200

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Tal talspektor on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var order = Order()
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select you cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add erxtra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
